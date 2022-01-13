480492c271986d22aa6db17827e5e9391e83eb83
Peter Blanchfield <zygomy@gmail.com>
2022-01-13 15:01:20 +0000

    New User File Works
    We can use the default file to create an active user or a new user and activate the appropriate buttons.

--- SimpleCardGame/Assets/GameStartScript.cs
+++ SimpleCardGame/Assets/GameStartScript.cs
@@ -7,17 +7,30 @@ using UnityEngine.UI;
 public class GameStartScript : MonoBehaviour
 {
     public Text userMessage;
+    public GameObject gameStart;
+    public GameObject gameRules;
+    public GameObject newUser;
     TextReader readUserData = new StreamReader("C:/Users/peter/Desktop/userdata.txt");
     // Start is called before the first frame update
     void Start()
     {
         string lineRead = readUserData.ReadLine();
+        gameStart.SetActive(false);
+        gameRules.SetActive(false);
+        newUser.SetActive(false);
         if (lineRead.Contains("$default_user"))
         {
             userMessage.text = "Hello New User";
+            newUser.SetActive(true);
         }
         else
+        {
             userMessage.text = "Hello " + lineRead;
+            gameStart.SetActive(true);
+            gameRules.SetActive(true);
+            userMessage.text = "\n got here";
+        }
+        readUserData.Close();
     }
 
     // Update is called once per frame
